64ced10091d62f93fc6d47e2f042cac707e2d2b6
Joey Stilley <Joey.Stilley@gmail.com>
2021-08-20 14:59:02 -0500

    initial completion of the SwitchStatements assignment

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What was your favorite subject in school?");
+            string userInput = Console.ReadLine();
+
+            FavClass(userInput);  
+        }
+        public static void FavClass(string userInput)
+        {
+            switch (userInput)
+            {
+                case "Math":
+                    Console.WriteLine("Nerd!");
+                    break;
+                case "Science":
+                    Console.WriteLine("Dweeb!");
+                    break;
+                case "English":
+                    Console.WriteLine("Bookworm!");
+                    break;
+                case "History":
+                    Console.WriteLine("Loser!");
+                    break;
+                case "Home Ec":
+                    Console.WriteLine("You're kind of dumb huh?!");
+                    break;
+                default:
+                    Console.WriteLine("Cool kid right here. Hates school and is going nowhere!!!");
+                    break;
+            }
+        }
+
+
+    }
+}
